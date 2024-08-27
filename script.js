29d87b000746e856e6c78c6138830425c6cb2b82
xsea29 <kumarsagar2977@gmail.com>
2024-08-27 23:47:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.to(".imagecontainer", {
+  width: "100%",
+  stagger: 2,
+  ease: Expo.easeInOut,
+  stagger: 2,
+});
+
+gsap.to(".text h1", {
+  top: 0,
+  ease: Expo.easeInOut,
+  stagger: 2,
+});
+
+gsap.to(".text h1", {
+  top: "100px",
+  ease: Expo.easeInOut,
+  delay: 2,
+  stagger: 2,
+});
